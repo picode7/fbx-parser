b9a5d914027a98e2f7f9c3a28c85d2d19e984a7b
picode7 <9394244+picode7@users.noreply.github.com>
2021-02-09 22:25:02 +0100

    types update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { parseBinary } from './binary'
 export { parseText } from './ascii'
 export { FBXReader, FBXReaderNode } from './FBXReader'
-export { FBXData as FBX, FBXNode, FBXProperty } from './shared'
+export { FBXData, FBXNode, FBXProperty } from './shared'
